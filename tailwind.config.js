5d0e8f5e85863eadaa67addc997a527ab4a64d52
Zulnabil Alhadad <zulnabil.22@gmail.com>
2021-01-07 02:09:42 +0800

    fix: purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
